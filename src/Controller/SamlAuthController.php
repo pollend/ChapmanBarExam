724e880a03fa60a649a6d670d4cfeac62b71f8c8
Michael Pollind <polli104@mail.chapman.edu>
2019-07-30 12:37:37 -0700

    fixed auth controller for login

--- src/Controller/SamlAuthController.php
+++ src/Controller/SamlAuthController.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Gesdinet\JWTRefreshTokenBundle\EventListener\AttachRefreshTokenOnSuccessListener;
 use Gesdinet\JWTRefreshTokenBundle\Model\RefreshTokenManager;
 use Gesdinet\JWTRefreshTokenBundle\Model\RefreshTokenManagerInterface;
+use Illuminate\Support\Str;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use OneLogin\Saml2\Auth;
@@ -23,6 +24,17 @@ use Symfony\Component\Security\Core\Exception\AuthenticationException;
 
 class SamlAuthController extends  AbstractController
 {
+
+    static $TENANTID = 'http://schemas.microsoft.com/identity/claims/tenantid'; // 00000000-0000-0000-0000-000000000000
+    static $OBJECT_IDENTIFIER = 'http://schemas.microsoft.com/identity/claims/objectidentifier'; // // 00000000-0000-0000-0000-000000000000
+    static $DISPLAY_NAME = 'http://schemas.microsoft.com/identity/claims/displayname'; // <Last Name>, <First Name> (<occupation>)
+    static $IDENTITYPROVIDER = 'http://schemas.microsoft.com/identity/claims/identityprovider'; // ttps://sts.windows.net/<tenantid>
+    static $METHOD_IDENTIFICATION = 'http://schemas.microsoft.com/claims/authnmethodsreference'; // method of auth
+    static $GIVEN_NAME = 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname'; // <First Name>
+    static $SURNAME = 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname'; // <Last Name>
+    static $EMAIL_ADDRESS = 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'; // <EMAIL>
+    static $NAME = 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'; // <email>
+
 
     protected $entityManager;
     protected $jwtManager;
@@ -73,13 +85,15 @@ class SamlAuthController extends  AbstractController
 //              $this->logger->error($this->oneLoginAuth->getLastErrorReason());
             throw new AuthenticationException($this->auth->getLastErrorReason());
         }
-        
+
         $attributes = $this->oneLoginAuth->getAttributes();
-        if (!($user = $userRepository->findByEmail($attributes['mail']))) {
+        $user = null;
+        //no user exist to create a new user
+        if (!($user = $userRepository->findByEmail(Str::lower($attributes[SamlAuthController::$EMAIL_ADDRESS][0])))) {
             $user = new User();
-            $user->setEmail($attributes['mail']);
+            $user->setEmail(Str::lower($attributes[SamlAuthController::$EMAIL_ADDRESS][0]));
             $user->setRoles([User::ROLE_USER]);
-            $user->setUsername($attributes['cn'] . ' ' . $attributes['sn']);
+            $user->setUsername($attributes[SamlAuthController::$GIVEN_NAME][0] . ' ' . $attributes[SamlAuthController::$SURNAME][0]);
             // TODO: not using remember token
             $user->setRememberToken('asfnaiw3am3o');
             $this->entityManager->persist($user);
